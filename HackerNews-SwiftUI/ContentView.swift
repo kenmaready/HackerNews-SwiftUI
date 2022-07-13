864d9df779deb7edf88548669cbb584ac1d4b5f2
kenmaready <ken@maready.com>
2022-07-13 07:29:09 -0400

    Initial commit

--- HackerNews-SwiftUI/ContentView.swift
+++ HackerNews-SwiftUI/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(testPosts) { post in
+                HStack {
+                    Text(post.id)
+                    Text(post.title)
+                    Image(systemName: "newspaper.fill")
+                }
+            }
+            .navigationTitle("hacker world:")
+        }
     }
 }
 
---
+++ HackerNews-SwiftUI/Models/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  HackerNews-SwiftUI
+//
+//  Created by Ken Maready on 7/13/22.
+//
+
+import Foundation
