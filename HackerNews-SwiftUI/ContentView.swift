1a0fa48e1d5f6aaed15c3307b75b2886afe0bec6
kenmaready <ken@maready.com>
2022-07-12 22:38:52 -0400

    Initial Commit

---
+++ HackerNews-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews-SwiftUI
+//
+//  Created by Ken Maready on 7/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackerNews-SwiftUI/HackerNews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNews_SwiftUIApp.swift
+//  HackerNews-SwiftUI
+//
+//  Created by Ken Maready on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
