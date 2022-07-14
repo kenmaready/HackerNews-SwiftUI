2bdc3633cc01a014abbe922e5b843db144aaec29
kenmaready <ken@maready.com>
2022-07-14 08:29:22 -0400

    Added basic keyword search and refresh on pulldown of list

--- HackerNews-SwiftUI/Models/NetworkManager.swift
+++ HackerNews-SwiftUI/Models/NetworkManager.swift
@@ -10,9 +10,11 @@ import Foundation
 class NetworkManager: ObservableObject {
     
     @Published var posts = [Post]()
+    var baseUrl = "http://hn.algolia.com/api/v1/search?"
+    var defaultSearchTerms = "tags=front_page"
     
-    func fetchData() {
-        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+    func fetchData(searchTerms: String = "tags=front_page") {
+        if let url = URL(string: baseUrl + searchTerms) {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { data, response, error in
                 if error == nil {
--- HackerNews-SwiftUI/Views/ContentView.swift
+++ HackerNews-SwiftUI/Views/ContentView.swift
@@ -10,19 +10,39 @@ import SwiftUI
 struct ContentView: View {
     
     @ObservedObject var networkManager = NetworkManager()
+    @State private var keyword: String = ""
     
     var body: some View {
         NavigationView {
-            List(networkManager.posts) { post in
+            VStack {
                 HStack {
-                    NavigationLink( destination: PostDetailView(url: post.url)
-                    ) {
-                        Text(String(post.points)).font(.system(size: 10))
-                        Text(post.title).font(.system(size: 12))
+                    Text("Search:")
+                    TextField("Keyword", text: $keyword)
+                        .border(.secondary).textInputAutocapitalization(.never)
+                        .onSubmit {
+                            print("searching for: \(keyword)")
+                            networkManager.fetchData(searchTerms: "query=\(keyword)&tags=story")
+                            keyword = ""
+                        }
+                }.padding(.horizontal)
+                List(networkManager.posts) { post in
+                    HStack {
+                        NavigationLink( destination: PostDetailView(url: post.url)
+                        ) {
+                            Text(String(post.points)).font(.system(size: 10))
+                            Text(post.title).font(.system(size: 12))
+                        }
                     }
+                }.refreshable {
+                    // TODO: fix so that refresh will maintain
+                    // current keyword search if applicable
+                    // (currently refresh will default to
+                    // basic article search and forget
+                    // current keyword)
+                    networkManager.fetchData()
                 }
+                .navigationTitle("H4CK3R W0RLD:")
             }
-            .navigationTitle("H4CK3R W0RLD:")
         }
         .onAppear {
             self.networkManager.fetchData()
